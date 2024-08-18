947ae24dc6c3d6d1d7160da3bcadbe06e69e621f
Ferperemero86 <fernandoperezmerono@gmail.com>
2024-08-18 14:21:37 +0100

    Add Details query/handler that fetches a single activity based on id

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -17,7 +17,7 @@ namespace API.Controllers
 
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return Ok();
+            return await Mediator.Send(new Details.Query{Id = id});
         }
         
     }
--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63a4e51f4f8deb81eaf17e2c217e6a91d26dce0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44438b9bd0127377788f2dc9e46ad1b63bb4390e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
